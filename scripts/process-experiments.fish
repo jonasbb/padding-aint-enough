dadfa5dd67a058da5df02bfe896c3279a43612bd
Jonas Bushart <jonas@bushart.org>
2018-06-26 15:35:41 +0200

    Small script to process the json/dnstap messages after an experiment
    Mainly such that I do not have to remember which commands to execute

---
+++ scripts/process-experiments.fish
@@ -0,0 +1,28 @@
+#!/usr/bin/env fish
+
+if test (count $argv) -lt 1
+    echo "The required argument for the experiments directory is missing."
+    exit 1
+end
+# path to experiments folder
+set -l EXPERIMENTS (realpath "$argv[1]")
+
+if test ! \( -e "$EXPERIMENTS" -a -d "$EXPERIMENTS" \)
+    echo "The experiments directory at $EXPERIMENTS does not exist."
+    exit 1
+end
+
+# go to parent directory of script
+set -l SCRIPT (realpath (dirname (status --current-filename))/../)
+
+cargo build --release --bin encrypted-dns
+# search in experiments folder for all files with json extension and process them
+fd --extension json . "$EXPERIMENTS" --exec ./target/release/encrypted-dns '{}' \;
+
+# copy all files into a single images directory
+pushd $EXPERIMENTS
+rm -rf ./images/**
+mkdir --parents ./images/
+for f in ./*/**/*.svg
+    cp $f ./images/(echo $f | tr '/' '-' | tail -c+3)
+end
