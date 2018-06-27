160836b6058e58db3ca3eea4b7afd4ca5bd8f0b3
Jonas Bushart <jonas@bushart.org>
2018-06-27 11:26:08 +0200

    Prefetch more domains

--- scripts/prefetch-unbound.fish
+++ scripts/prefetch-unbound.fish
@@ -1,4 +1,12 @@
 #!/usr/bin/fish
 pushd (dirname (status --current-filename))
-../dns-par --servers "127.0.0.1" --file ./alexa-top1000-tlds.txt >/dev/null
+set -l PREFETCH_DOMAINS ./alexa-top30k-eff-tlds.txt
+set -l LINES (wc -l "$PREFETCH_DOMAINS" | cut -d ' ' -f 1)
+set -l BATCHSIZE 50
+
+for range in (seq 1 $BATCHSIZE $LINES)
+    # prefetch 100 entries at a time
+    ../dns-par --servers "127.0.0.1" --file (tail -n+"$range" "$PREFETCH_DOMAINS" | head -n"$BATCHSIZE" | psub) >/dev/null
+    sleep .5
+end
 sleep 2
