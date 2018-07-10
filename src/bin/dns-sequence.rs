998cebf9eb36b259884ac12915d90df8c02831f2
Jonas Bushart <jonas@bushart.org>
2018-07-10 17:16:04 +0200

    Add some tests for the classification

--- src/bin/dns-sequence.rs
+++ src/bin/dns-sequence.rs
@@ -647,3 +647,106 @@ fn classify_sequence(sequence: &Sequence) -> Option<&'static str> {
     }
 }
 
+#[test]
+fn test_classify_sequence_r001() {
+    use SequenceElement::{Gap, Size};
+
+    let sequence = Sequence::new(vec![Size(1), Size(2)], "".to_string());
+    assert_eq!(classify_sequence(&sequence), Some(R001));
+
+    let sequence = Sequence::new(vec![Size(1), Gap(3), Size(2)], "".to_string());
+    assert_eq!(classify_sequence(&sequence), Some(R001));
+
+    let sequence = Sequence::new(vec![Size(1), Gap(10), Size(2)], "".to_string());
+    assert_eq!(classify_sequence(&sequence), Some(R001));
+
+    let sequence = Sequence::new(vec![Gap(9), Size(1), Size(2), Gap(12)], "".to_string());
+    assert_eq!(classify_sequence(&sequence), Some(R001));
+
+    let sequence = Sequence::new(
+        vec![Gap(9), Size(1), Gap(5), Size(2), Gap(12)],
+        "".to_string(),
+    );
+    assert_eq!(classify_sequence(&sequence), Some(R001));
+}
+
+#[test]
+fn test_classify_sequence_r002() {
+    use SequenceElement::{Gap, Size};
+
+    let sequence = Sequence::new(vec![Size(1), Size(2), Size(1)], "".to_string());
+    assert_eq!(classify_sequence(&sequence), Some(R002));
+
+    let sequence = Sequence::new(vec![Size(1), Gap(3), Size(2), Size(1)], "".to_string());
+    assert_eq!(classify_sequence(&sequence), Some(R002));
+
+    let sequence = Sequence::new(
+        vec![Size(1), Gap(5), Size(2), Gap(10), Size(1)],
+        "".to_string(),
+    );
+    assert_eq!(classify_sequence(&sequence), Some(R002));
+
+    let sequence = Sequence::new(
+        vec![Gap(2), Size(1), Gap(15), Size(2), Gap(2), Size(1), Gap(3)],
+        "".to_string(),
+    );
+    assert_eq!(classify_sequence(&sequence), Some(R002));
+
+    // These must fail
+
+    let sequence = Sequence::new(vec![Size(1), Size(1), Size(1)], "".to_string());
+    assert_ne!(classify_sequence(&sequence), Some(R002));
+
+    let sequence = Sequence::new(vec![Size(1), Size(1), Size(2)], "".to_string());
+    assert_ne!(classify_sequence(&sequence), Some(R002));
+
+    let sequence = Sequence::new(vec![Size(2), Size(1), Size(1)], "".to_string());
+    assert_ne!(classify_sequence(&sequence), Some(R002));
+}
+
+#[test]
+fn test_classify_sequence_r007() {
+    use SequenceElement::{Gap, Size};
+
+    let sequence = Sequence::new(vec![Size(1), Gap(3), Size(1)], "".to_string());
+    assert_eq!(classify_sequence(&sequence), Some(R007));
+
+    let sequence = Sequence::new(
+        vec![Size(1), Gap(3), Size(1), Gap(3), Size(1), Gap(3), Size(1)],
+        "".to_string(),
+    );
+    assert_eq!(classify_sequence(&sequence), Some(R007));
+
+    // These must fail
+
+    let sequence = Sequence::new(
+        vec![
+            Size(1),
+            Gap(2),
+            Size(2),
+            Gap(9),
+            Size(1),
+            Gap(2),
+            Size(1),
+            Gap(9),
+            Size(1),
+            Gap(2),
+            Size(1),
+            Gap(1),
+            Size(1),
+            Gap(2),
+            Size(1),
+            Gap(8),
+            Size(1),
+            Gap(2),
+            Size(1),
+            Gap(2),
+            Size(2),
+            Gap(2),
+            Size(2),
+        ],
+        "".to_string(),
+    );
+
+    assert_ne!(classify_sequence(&sequence), Some(R007));
+}
