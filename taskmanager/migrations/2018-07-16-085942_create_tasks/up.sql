99119c3c0652050493d8802286d1e0cc1a3343e2
Jonas Bushart <jonas@bushart.org>
2018-07-17 17:05:05 +0200

    Update schema to allow storing messages

--- taskmanager/migrations/2018-07-16-085942_create_tasks/up.sql
+++ taskmanager/migrations/2018-07-16-085942_create_tasks/up.sql
@@ -10,3 +10,12 @@ CREATE TABLE tasks (
     last_modified DATETIME NOT NULL,
     associated_data TEXT
 );
+
+CREATE TABLE infos (
+    id INTEGER NOT NULL PRIMARY KEY,
+    task_id INTEGER NOT NULL,
+    time DATETIME NOT NULL,
+    message TEXT NOT NULL,
+
+    FOREIGN KEY (task_id) REFERENCES tasks(id)
+);
