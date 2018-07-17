359d4c1567670f497293d02b25d950e9a9490743
Jonas Bushart <jonas@bushart.org>
2018-07-17 13:54:36 +0200

    Move function which for dnstap2sequence into library

--- src/lib.rs
+++ src/lib.rs
@@ -20,13 +20,16 @@ extern crate trust_dns;
 pub mod protos;
 pub mod sequences;
 
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Duration, Utc};
+use dnstap::Message_Type;
 use failure::{Error, ResultExt};
 use framestream::DecoderReader;
 use min_max_heap::MinMaxHeap;
 use misc_utils::fs::file_open_read;
 pub use protos::dnstap;
-use std::{convert::TryFrom, path::Path};
+use protos::DnstapContent;
+use sequences::{Sequence, SequenceElement};
+use std::{collections::BTreeMap, convert::TryFrom, path::Path};
 
 pub fn process_dnstap<P: AsRef<Path>>(
     path: P,
@@ -173,4 +176,223 @@ pub mod common_sequence_classifications {
     pub const R006_3RD_LVL_DOM: &str =
         "R006 www redirect + Akamai on 3rd-LVL domain without DNSSEC";
     pub const R007: &str = "R007 Unreachable Name Server";
+}
+
+/// Load a dnstap file and generate a Sequence from it
+pub fn dnstap_to_sequence(dnstap_file: &Path) -> Result<Sequence, Error> {
+    // process dnstap if available
+    let mut events: Vec<protos::Dnstap> =
+        process_dnstap(&*dnstap_file)?.collect::<Result<_, Error>>()?;
+
+    // the dnstap events can be out of order, so sort them by timestamp
+    // always take the later timestamp if there are multiple
+    events.sort_by_key(|ev| {
+        let DnstapContent::Message {
+            query_time,
+            response_time,
+            ..
+        } = ev.content;
+        if let Some(time) = response_time {
+            return time;
+        } else if let Some(time) = query_time {
+            return time;
+        } else {
+            panic!("The dnstap message must contain either a query or response time.")
+        }
+    });
+
+    let mut unanswered_client_queries: BTreeMap<MatchKey, UnmatchedClientQuery> = BTreeMap::new();
+    let mut matched = Vec::new();
+
+    for ev in events
+            .into_iter()
+                // search for the CLIENT_RESPONE `start.example.` message as the end of the prefetching events
+            .skip_while(|ev| {
+                let DnstapContent::Message {
+                    message_type,
+                    ref response_message,
+                    ..
+                } = ev.content;
+                if message_type == Message_Type::CLIENT_RESPONSE {
+                    let (dnsmsg, _size) =
+                        response_message.as_ref().expect("Unbound always sets this");
+                    let qname = dnsmsg.queries()[0].name().to_utf8();
+                    if qname == "start.example." {
+                        return false;
+                    }
+                }
+                true
+            })
+            // the skip while returns the CLIENT_RESPONSE with `start.example.`
+            // We want to remove this as well, so skip over the first element here
+            .skip(1)
+            // Only process messages until the end message is found in form of the first (thus CLIENT_QUERY)
+            // message forr domain `end.example.`
+            .take_while(|ev| {
+                let DnstapContent::Message {
+                    message_type,
+                    ref query_message,
+                    ..
+                } = ev.content;
+                if message_type == Message_Type::CLIENT_QUERY {
+                    let (dnsmsg, _size) =
+                        query_message.as_ref().expect("Unbound always sets this");
+                    let qname = dnsmsg.queries()[0].name().to_utf8();
+                    if qname == "end.example." {
+                        return false;
+                    }
+                }
+                true
+            }) {
+        let DnstapContent::Message {
+            message_type,
+            query_message,
+            response_message,
+            query_time,
+            response_time,
+            ..
+        } = ev.content;
+        match message_type {
+            Message_Type::FORWARDER_QUERY => {
+                let (dnsmsg, size) = query_message.expect("Unbound always sets this");
+                let qname = dnsmsg.queries()[0].name().to_utf8();
+                let qtype = dnsmsg.queries()[0].query_type().to_string();
+                let id = dnsmsg.id();
+                let start = query_time.expect("Unbound always sets this");
+
+                let key = MatchKey {
+                    qname: qname.clone(),
+                    qtype: qtype.clone(),
+                    id,
+                    port: 0,
+                };
+                let value = UnmatchedClientQuery {
+                    qname,
+                    qtype,
+                    start,
+                    size: size as u32,
+                };
+                let existing_value = unanswered_client_queries.insert(key, value);
+                if let Some(existing_value) = existing_value {
+                    info!(
+                        "Duplicate Forwarder Query for '{}' ({})",
+                        existing_value.qname, existing_value.qtype
+                    );
+                }
+            }
+
+            Message_Type::FORWARDER_RESPONSE => {
+                let (dnsmsg, size) = response_message.expect("Unbound always sets this: FR r msg");
+                let qname = dnsmsg.queries()[0].name().to_utf8();
+                let qtype = dnsmsg.queries()[0].query_type().to_string();
+                let start = query_time.expect("Unbound always sets this: FR q time");
+                let id = dnsmsg.id();
+                let end = response_time.expect("Unbound always sets this: FR r time");
+
+                let key = MatchKey {
+                    qname: qname.clone(),
+                    qtype: qtype.clone(),
+                    id,
+                    port: 0,
+                };
+                if let Some(unmatched) = unanswered_client_queries.remove(&key) {
+                    matched.push(Query {
+                        source: QuerySource::Forwarder,
+                        qname,
+                        qtype,
+                        start,
+                        end,
+                        query_size: unmatched.size,
+                        response_size: size as u32,
+                    });
+                } else {
+                    info!("Unmatched Forwarder Response for '{}' ({})", qname, qtype);
+                };
+            }
+
+            _ => {}
+        }
+    }
+
+    // cleanup some messages
+    // filter out all the queries which are just noise
+    matched.retain(|query| {
+        !(query.qtype == "NULL" && query.qname.starts_with("_ta"))
+            && query.qname != "fedoraproject.org."
+    });
+    for msg in unanswered_client_queries {
+        debug!("Unanswered forwarder query: {:?}", msg);
+    }
+    // the values are not necessarily in correct order, thus sort them here by end time
+    // end time is the time when the response arrives, which is the most interesting field for the attacker
+    matched.sort_by_key(|x| x.end);
+
+    let seq = convert_to_sequence(&matched, dnstap_file.to_string_lossy().to_string());
+    Ok(seq.ok_or_else(|| format_err!("Sequence is empty"))?)
+}
+
+/// Takes a list of Queries and returns a Sequence
+///
+/// The functions abstracts over some details of Queries, such as absolute size and absolute time.
+fn convert_to_sequence(data: &[Query], identifier: String) -> Option<Sequence> {
+    let base_gap_size = Duration::microseconds(1000);
+
+    if data.is_empty() {
+        return None;
+    }
+
+    let mut last_end = None;
+    Some(Sequence::new(
+        data.into_iter()
+            .flat_map(|d| {
+                let mut gap = None;
+                if let Some(last_end) = last_end {
+                    gap = gap_size(d.end - last_end, base_gap_size);
+                }
+                last_end = Some(d.end);
+
+                let size = pad_size(d.response_size, false, Padding::Q128R468);
+                gap.into_iter().chain(Some(size))
+            })
+            .collect(),
+        identifier,
+    ))
+}
+
+fn gap_size(gap: Duration, base: Duration) -> Option<SequenceElement> {
+    if gap <= base {
+        return None;
+    }
+    let mut gap = gap;
+    let mut out = 0;
+    while gap > base {
+        gap = gap - base;
+        out += 1;
+    }
+    let dist = f64::from(out).log2() as u8;
+    if dist == 0 {
+        None
+    } else {
+        SequenceElement::Gap(dist).into()
+    }
+}
+
+fn pad_size(size: u32, is_query: bool, padding: Padding) -> SequenceElement {
+    use Padding::*;
+    SequenceElement::Size(match (padding, is_query) {
+        (Q128R468, true) => block_padding(size, 128) / 128,
+        (Q128R468, false) => block_padding(size, 468) / 468,
+    } as u8)
+}
+
+fn block_padding(size: u32, block_size: u32) -> u32 {
+    if size % block_size == 0 {
+        size
+    } else {
+        size / block_size * block_size + block_size
+    }
+}
+
+enum Padding {
+    Q128R468,
 }
