d010ff537c8d08efc43ea4aeb5d584820fde0aba
Jonas Bushart <jonas@bushart.org>
2018-07-18 16:33:01 +0200

    Countless changes to the taskmanager

--- taskmanager/src/lib.rs
+++ taskmanager/src/lib.rs
@@ -18,13 +18,36 @@ use diesel::{prelude::*, sqlite::SqliteConnection};
 use failure::{Error, ResultExt};
 use misc_utils::fs::file_open_read;
 use std::{
-    fmt::{self, Debug},
+    fmt::{self, Debug, Display},
     path::{Path, PathBuf},
     sync::{Arc, Mutex},
 };
 
 pub mod models;
 pub mod schema;
+
+type TasksColumnType = (
+    schema::tasks::id,
+    schema::tasks::priority,
+    schema::tasks::name,
+    schema::tasks::domain,
+    schema::tasks::domain_counter,
+    schema::tasks::state,
+    schema::tasks::restart_count,
+    schema::tasks::last_modified,
+    schema::tasks::associated_data,
+);
+const TASKS_COLUMNS: TasksColumnType = (
+    schema::tasks::id,
+    schema::tasks::priority,
+    schema::tasks::name,
+    schema::tasks::domain,
+    schema::tasks::domain_counter,
+    schema::tasks::state,
+    schema::tasks::restart_count,
+    schema::tasks::last_modified,
+    schema::tasks::associated_data,
+);
 
 #[derive(Clone)]
 pub struct TaskManager {
@@ -45,12 +68,29 @@ impl TaskManager {
         Ok(Self { db_connection })
     }
 
-    pub fn delete_all_tasks(&self) -> Result<(), Error> {
+    pub fn delete_all(&self) -> Result<(), Error> {
         let conn = self.db_connection.lock().unwrap();
         conn.transaction::<(), _, _>(|| {
             diesel::delete(schema::tasks::table)
                 .execute(&*conn)
                 .context("Trying to delete `tasks` table")?;
+            diesel::delete(schema::infos::table)
+                .execute(&*conn)
+                .context("Trying to delete `infos` table")?;
+            Ok(())
+        })
+    }
+
+    fn update_single_task(
+        &self,
+        conn: &SqliteConnection,
+        task: &models::Task,
+    ) -> Result<(), Error> {
+        conn.transaction::<(), Error, _>(|| {
+            diesel::update(task)
+                .set(task)
+                .execute(conn)
+                .context("Cannot update task")?;
             Ok(())
         })
     }
@@ -89,14 +129,16 @@ impl TaskManager {
 
     /// Return a task which waits for a VM to be executed
     pub fn get_task_for_vm(&self, executor: &Executor) -> Result<Option<models::Task>, Error> {
-        use schema::tasks::dsl::{priority, state, tasks};
+        use schema::tasks::dsl::{aborted, priority, state, tasks};
 
         let conn = self.db_connection.lock().unwrap();
         conn.transaction::<Option<models::Task>, _, _>(|| {
             let res = tasks
                 .filter(state.eq(models::TaskState::Created))
+                .filter(aborted.eq(false))
                 .order_by(priority.asc())
                 .limit(1)
+                .select(TASKS_COLUMNS)
                 .load::<models::Task>(&*conn)
                 .context("Cannot retrieve task from database")?;
 
@@ -139,24 +181,20 @@ impl TaskManager {
         );
 
         let conn = self.db_connection.lock().unwrap();
-        conn.transaction::<(), _, _>(|| {
-            diesel::update(&*task)
-                .set(&*task)
-                .execute(&*conn)
-                .context("Cannot update task")?;
-            Ok(())
-        })
+        self.update_single_task(&*conn, task)
     }
 
     pub fn results_collectable(
         &self,
     ) -> Result<Vec<(models::Task, ResultsCollectableData)>, Error> {
-        use schema::tasks::dsl::{priority, state, tasks};
+        use schema::tasks::dsl::{aborted, priority, state, tasks};
 
         let conn = self.db_connection.lock().unwrap();
         Ok(tasks
             .filter(state.eq(models::TaskState::ResultsCollectable))
+            .filter(aborted.eq(false))
             .order_by(priority.asc())
+            .select(TASKS_COLUMNS)
             .load::<models::Task>(&*conn)
             .context("Cannot retrieve tasks from database")?
             .into_iter()
@@ -168,6 +206,135 @@ impl TaskManager {
                 Ok((task, data))
             })
             .collect::<Result<Vec<_>, Error>>()?)
+    }
+
+    pub fn mark_results_collected(&self, task: &mut models::Task) -> Result<(), Error> {
+        if task.state != models::TaskState::ResultsCollectable {
+            bail!("Mark results as collected the task must be in the ResultsCollectable state.")
+        }
+
+        // Update task state
+        task.advance();
+        task.associated_data = None;
+
+        let conn = self.db_connection.lock().unwrap();
+        self.update_single_task(&*conn, task)
+    }
+
+    pub fn results_need_sanity_check_single(&self) -> Result<Vec<models::Task>, Error> {
+        use schema::tasks::dsl::{aborted, priority, state, tasks};
+
+        let conn = self.db_connection.lock().unwrap();
+        Ok(tasks
+            .filter(state.eq(models::TaskState::CheckQualitySingle))
+            .filter(aborted.eq(false))
+            .order_by(priority.asc())
+            .select(TASKS_COLUMNS)
+            .load::<models::Task>(&*conn)
+            .context("Cannot retrieve tasks from database")?)
+    }
+
+    pub fn mark_results_checked_single(&self, task: &mut models::Task) -> Result<(), Error> {
+        if task.state != models::TaskState::CheckQualitySingle {
+            bail!("To check results they must be in the CheckQualitySingle state.")
+        }
+
+        // Update task state
+        task.advance();
+        task.associated_data = None;
+
+        let conn = self.db_connection.lock().unwrap();
+        self.update_single_task(&*conn, task)
+    }
+
+    pub fn results_need_sanity_check_domain(&self) -> Result<Vec<models::Task>, Error> {
+        use schema::tasks::dsl::{aborted, priority, state, tasks};
+
+        let conn = self.db_connection.lock().unwrap();
+        Ok(tasks
+            .filter(state.eq(models::TaskState::CheckQualityDomain))
+            .filter(aborted.eq(false))
+            .order_by(priority.asc())
+            .select(TASKS_COLUMNS)
+            .load::<models::Task>(&*conn)
+            .context("Cannot retrieve tasks from database")?)
+    }
+
+    pub fn mark_results_checked_domain(&self, tasks: Vec<&mut models::Task>) -> Result<(), Error> {
+        unimplemented!()
+        // if task.state != models::TaskState::CheckQualitySingle {
+        //     bail!("To check results they must be in the CheckQualitySingle state.")
+        // }
+
+        // // Update task state
+        // task.advance();
+        // task.associated_data = None;
+
+        // self.update_single_task(task)
+    }
+
+    pub fn restart_task(&self, task: &mut models::Task, reason: &Display) -> Result<(), Error> {
+        task.restart();
+        task.associated_data = None;
+
+        let conn = self.db_connection.lock().unwrap();
+
+        if task.restart_count() < 4 {
+            // The task is still allowed to be restarted
+            let msg = format!("Restart task {} because {}", task.name(), reason);
+            conn.transaction::<(), _, _>(|| {
+                self.update_single_task(&*conn, task)?;
+
+                let row = models::InfoInsert {
+                    id: None,
+                    task_id: task.id(),
+                    time: Local::now().naive_local(),
+                    message: &*msg,
+                };
+                diesel::insert_into(schema::infos::table)
+                    .values(&row)
+                    .execute(&*conn)
+                    .context("Error creating new task")?;
+                Ok(())
+            })
+        } else {
+            use schema::tasks::dsl::{domain, tasks};
+
+            // We must abort all tasks for this domain
+            let msg = format!("Too many restarts for task {}, abort domain.", task.name());
+
+            conn.transaction::<(), _, _>(|| {
+                // get all tasks for the same domain
+                let other_tasks = tasks
+                    .filter(domain.eq(task.domain()))
+                    .select(TASKS_COLUMNS)
+                    .load::<models::Task>(&*conn)
+                    .context("Cannot retrieve task from database")?;
+
+                for mut other_task in other_tasks {
+                    let abort_task = other_task.abort(&msg);
+                    diesel::update(&abort_task)
+                        .set(&abort_task)
+                        .execute(&*conn)
+                        .context("Cannot update task")?;
+                    let row = models::InfoInsert {
+                        id: None,
+                        task_id: other_task.id(),
+                        time: Local::now().naive_local(),
+                        message: &*msg,
+                    };
+                    diesel::insert_into(schema::infos::table)
+                        .values(&row)
+                        .execute(&*conn)
+                        .context("Error creating new task")?;
+                }
+                Ok(())
+            })
+        }
+    }
+
+    pub fn find_stale_tasks(&self) -> Result<Vec<models::Task>, Error> {
+        unimplemented!()
     }
 }
 
@@ -189,6 +356,7 @@ pub struct Config {
 pub struct Executor {
     pub name: String,
     pub sshconnect: String,
+    pub working_directory: PathBuf,
 }
 
 impl Config {
@@ -202,5 +370,13 @@ impl Config {
 
     pub fn get_database_path(&self) -> PathBuf {
         self.database.clone()
+    }
+
+    pub fn get_collected_results_path(&self) -> PathBuf {
+        self.working_directory.join("unprocessed")
+    }
+
+    pub fn get_scripts_dir(&self) -> PathBuf {
+        self.working_directory.join("scripts")
     }
 }
--- taskmanager/src/main.rs
+++ taskmanager/src/main.rs
@@ -1,17 +1,26 @@
+#![allow(proc_macro_derive_resolution_fallback)]
+#![allow(dead_code)]
+
+extern crate encrypted_dns;
 extern crate env_logger;
 #[macro_use]
 extern crate failure;
 #[macro_use]
+extern crate lazy_static;
+#[macro_use]
 extern crate log;
 extern crate misc_utils;
+extern crate rayon;
 #[macro_use]
 extern crate structopt;
 extern crate serde;
 extern crate taskmanager;
 extern crate toml;
 
+use encrypted_dns::{dnstap_to_sequence, ErrorExt};
 use failure::{Error, ResultExt};
 use misc_utils::fs::file_open_read;
+use rayon::prelude::*;
 use std::{
     ffi::{OsStr, OsString},
     fmt::{self, Debug},
@@ -19,12 +28,18 @@ use std::{
     io::{self, BufRead, BufReader, Read},
     panic::{self, RefUnwindSafe, UnwindSafe},
     path::{Path, PathBuf},
-    process::Command,
+    process::{Command, Stdio},
+    sync::Arc,
     thread::{self, JoinHandle},
     time::Duration,
 };
 use structopt::StructOpt;
-use taskmanager::*;
+use taskmanager::{models::Task, *};
+
+lazy_static! {
+    static ref DNSTAP_FILE_NAME: &'static Path = &Path::new("website-log.dnstap.xz");
+    static ref CHROME_LOG_FILE_NAME: &'static Path = &Path::new("website-log.json.xz");
+}
 
 #[derive(StructOpt)]
 #[structopt(
@@ -146,7 +161,7 @@ fn run_init(cmd: SubCommand, config: Config) -> Result<(), Error> {
             .collect::<Result<Vec<String>, std::io::Error>>()
             .with_context(|_| format!("Failed to read line in {}", domain_list_path.display()))?;
         taskmgr
-            .delete_all_tasks()
+            .delete_all()
             .context("Empty database before filling it")?;
         taskmgr
             .add_domains(domains, config.per_domain_datasets)
@@ -162,16 +177,27 @@ fn run_exec(cmd: SubCommand, config: Config) -> Result<(), Error> {
     if let SubCommand::Run = cmd {
         let mut taskmgr = TaskManager::new(&*config.get_database_path().to_string_lossy())
             .context("Cannot create TaskManager")?;
+        let config = Arc::new(config);
 
         if config.executors.is_empty() {
             bail!("You need to specify at least one executor.");
         }
 
-        // taskmgr
-        //     .get_task_for_vm(&config.executors[0])
-        //     .context("Could not create tasks")?;
+        if !config.get_scripts_dir().exists() {
+            bail!(
+                "The local directory with scripts does not exist!\nMissing {}",
+                config.get_scripts_dir().display()
+            );
+        }
 
         let mut handles = Vec::new();
+
+        &config
+            .executors
+            .par_iter()
+            .map(|executor| init_vm(executor, &config))
+            .collect::<Result<(), Error>>()
+            .context("Could not initialize all VMs")?;
 
         for executor in &config.executors {
             let executor_ = executor.clone();
@@ -184,9 +210,16 @@ fn run_exec(cmd: SubCommand, config: Config) -> Result<(), Error> {
 
         {
             let taskmgr_ = taskmgr.clone();
+            let config_ = config.clone();
             handles.push(run_thread_restart(
-                move || copy_vm_results(&taskmgr_),
+                move || copy_vm_results(&taskmgr_, &config_),
                 Some("Results Collector".to_string()),
+            ));
+            let taskmgr_ = taskmgr.clone();
+            let config_ = config.clone();
+            handles.push(run_thread_restart(
+                move || result_sanity_checks(&taskmgr_, &config_),
+                Some("Sanity Check Single".to_string()),
             ));
         }
 
@@ -211,23 +244,26 @@ fn run_debug(args: CliArgs, config: Config) -> Result<(), Error> {
 ///
 /// This is a small wrapper around `thread::spawn`, which ensures that if a thread panics or the
 /// function returns it is restarted.
-fn run_thread_restart<F, R>(function: F, name: Option<String>) -> JoinHandle<()>
+fn run_thread_restart<F>(function: F, name: Option<String>) -> JoinHandle<()>
 where
-    F: Fn() -> R + UnwindSafe + RefUnwindSafe + Send + 'static,
+    F: UnwindSafe + RefUnwindSafe + Send + 'static,
+    F: Fn() -> Result<(), Error>,
 {
     let mut builder = thread::Builder::new();
-    if let Some(name) = name {
-        builder = builder.name(name);
+    if let Some(name) = &name {
+        builder = builder.name(name.clone());
     }
 
     builder
         .spawn(move || loop {
-            if let Err(err) = panic::catch_unwind(&function) {
-                if let Some(err) = err.downcast_ref::<Error>() {
-                    error!("{}", err);
-                }
+            let res = panic::catch_unwind(&function);
+            if let Ok(Err(err)) = res {
+                error!("{}", err);
             }
-            eprintln!("Thread stopped, restart");
+            error!(
+                "Thread {} stopped, restart",
+                name.as_ref().map(|s| &**s).unwrap_or("<unknown>")
+            );
             thread::sleep(Duration::new(1, 0));
         })
         .unwrap()
@@ -237,47 +273,231 @@ where
 ///
 /// This function is responsible for all the steps related to capturing the measurement data from
 /// the VMs.
-fn process_tasks_vm(taskmgr: &TaskManager, executor: &Executor) {
+fn process_tasks_vm(taskmgr: &TaskManager, executor: &Executor) -> Result<(), Error> {
     loop {
-        if let Some(mut task) = taskmgr.get_task_for_vm(executor).unwrap() {
-            println!("Process task {}", task.id());
-            thread::sleep(Duration::new(2, 0));
-            println!("Advance task {}", task.id());
-            taskmgr.finished_task_for_vm(&mut task, &Path::new("/"));
+        if let Some(mut task) = taskmgr.get_task_for_vm(executor)? {
+            execute_or_restart_task(&mut task, taskmgr, |mut task| {
+                info!("Process task {}, step VM", task.id());
+                let script_path = executor
+                    .working_directory
+                    .join("scripts")
+                    .join("record-websites.fish");
+                let output_path = executor.working_directory.join(task.name());
+                let logfile = output_path.join("task.log");
+
+                // Run task on VM
+                // Execute: mkdir -p OUTPUT_PATH && cd OUTPUT_PATH && fish ./SCRIPT_PATH DOMAIN >LOGFILE 2>&1
+                let res = Command::new("ssh")
+                    .args(&[&executor.sshconnect, "--", "mkdir", "-p"])
+                    .arg(&output_path)
+                    .args(&["&&", "cd"])
+                    .arg(&output_path)
+                    .args(&["&&", "fish"])
+                    .arg(&script_path)
+                    .arg(&format!("http://{}/", task.domain()))
+                    .arg(">")
+                    .arg(&logfile)
+                    .arg("2>&1")
+                    .status()
+                    .with_context(|_| {
+                        format!(
+                            "Could not run script for task {} on VM {}",
+                            task.name(),
+                            executor.name,
+                        )
+                    })?;
+                if !res.success() {
+                    bail!(
+                        "Could not run script for task {} on VM {}",
+                        task.name(),
+                        executor.name,
+                    )
+                }
+
+                taskmgr.finished_task_for_vm(&mut task, &output_path)
+            })?;
         } else {
-            println!("No tasks left");
-            thread::sleep(Duration::new(2, 0));
+            info!("No tasks left for VM");
+            thread::sleep(Duration::new(10, 0));
         }
     }
 }
 
 /// Cleanup stale tasks by resetting them
-fn cleanup_stale_tasks(taskmgr: &TaskManager) {
+fn cleanup_stale_tasks(_taskmgr: &TaskManager) {
     // TODO restart counter !!!
     unimplemented!()
 }
 
 /// Copy the finished results from a VM to the global directory
-fn copy_vm_results(taskmgr: &TaskManager) -> Result<(), Error> {
-    let tasks = taskmgr.results_collectable()?;
-    for (task, data) in tasks {
-        // copy data from VM
-        Command::new("scp").args(&[
-            "-pr",
-            &format!("{}:{}", data.executor.sshconnect, data.path_on_vm.display()),
-        ]);
+fn copy_vm_results(taskmgr: &TaskManager, config: &Config) -> Result<(), Error> {
+    let local_path = config.get_collected_results_path();
+    loop {
+        ensure_path_exists(&local_path).context("Cannot create local path for collected results")?;
+        let tasks = taskmgr.results_collectable()?;
+        for (mut task, data) in tasks {
+            execute_or_restart_task(&mut task, taskmgr, |mut task| {
+                // copy data from VM to local directory
+                scp_file(
+                    &data.executor,
+                    ScpDirection::RemoteToLocal,
+                    &local_path,
+                    &data.path_on_vm,
+                )?;
+                // copy data from VM to local directory
+                let res = Command::new("ssh")
+                    .args(&[&data.executor.sshconnect, "--", "rm", "-rf"])
+                    .arg(&data.path_on_vm)
+                    .status()
+                    .with_context(|_| {
+                        format!(
+                            "Could not copy the results of {} from VM {}",
+                            task.name(),
+                            data.executor.name,
+                        )
+                    })?;
+                if !res.success() {
+                    bail!(
+                        "Could not copy the results of {} from VM {}",
+                        task.name(),
+                        data.executor.name,
+                    )
+                }
+
+                // compress files to save space
+                for entry in fs::read_dir(local_path.join(task.name()))? {
+                    let entry = entry?;
+                    if let Ok(file_type) = entry.file_type() {
+                        if file_type.is_file() {
+                            let path = entry.path();
+                            xz(&*path).with_context(|_| {
+                                format!("Failed to compress {}", path.display())
+                            })?;
+                        }
+                    }
+                }
+
+                taskmgr.mark_results_collected(&mut task)
+            })?;
+        }
+        thread::sleep(Duration::new(1, 0));
+    }
+}
+
+/// Check the VM results for consistency
+fn result_sanity_checks(taskmgr: &TaskManager, config: &Config) -> Result<(), Error> {
+    let local_path = config.get_collected_results_path();
+
+    loop {
+        let tasks = taskmgr.results_need_sanity_check_single()?;
+        for mut task in tasks {
+            execute_or_restart_task(&mut task, taskmgr, |mut task| {
+                // if a file is loadable, it passes all easy sanity checks
+                dnstap_to_sequence(&local_path.join(task.name()).join(&*DNSTAP_FILE_NAME))
+                    .with_context(|_| {
+                        format!(
+                            "The task {} generated invalid data and gets restarted.",
+                            task.name()
+                        )
+                    })?;
+                taskmgr.mark_results_checked_single(&mut task)
+            })?;
+        }
+        thread::sleep(Duration::new(1, 0));
+    }
+}
+
+/// Ensure the given path exists and if not create it
+fn ensure_path_exists(path: &Path) -> io::Result<()> {
+    if !path.exists() {
+        fs::create_dir_all(path)?;
     }
     Ok(())
 }
 
-/// Check the VM results for consistency
-fn result_sanity_checks(taskmgr: &TaskManager) {
-    unimplemented!()
+enum ScpDirection {
+    LocalToRemote,
+    RemoteToLocal,
 }
 
-fn ensure_path_exists(path: &Path) -> io::Result<()> {
-    if !path.exists() {
-        fs::create_dir_all(path)?;
+/// Copy files between local and remote in both directions
+fn scp_file(
+    executor: &Executor,
+    direction: ScpDirection,
+    local_path: &Path,
+    remote_path: &Path,
+) -> Result<(), Error> {
+    let mut cmd = Command::new("scp");
+    cmd.arg("-pr");
+    let mut remote =
+        OsString::with_capacity(executor.name.len() + remote_path.as_os_str().len() + 4);
+    remote.push(&executor.name);
+    remote.push(":");
+    remote.push(remote_path);
+    match direction {
+        ScpDirection::LocalToRemote => cmd.args(&[local_path.as_os_str(), &*remote]),
+        ScpDirection::RemoteToLocal => cmd.args(&[&*remote, local_path.as_os_str()]),
+    };
+    let res = cmd
+        .stdin(Stdio::null())
+        .stdout(Stdio::null())
+        .status()
+        .context("Could not start scp")?;
+    if !res.success() {
+        bail!("scp did not finish successfully")
+    }
+    Ok(())
+}
+
+/// Make sure all necessary files are copied to the VM
+fn init_vm(executor: &Executor, config: &Config) -> Result<(), Error> {
+    let res = Command::new("ssh")
+        .arg(&executor.sshconnect)
+        .arg("--")
+        .arg("mkdir")
+        .arg("-p")
+        .arg(&executor.working_directory)
+        .status()
+        .with_context(|_| format!("Could not create working dir on VM {}", executor.name,))?;
+    if !res.success() {
+        bail!("Could not create working dir on VM {}", executor.name,)
+    }
+    scp_file(
+        executor,
+        ScpDirection::LocalToRemote,
+        &config.get_scripts_dir(),
+        &executor.working_directory,
+    )
+}
+
+fn execute_or_restart_task<F>(task: &mut Task, taskmgr: &TaskManager, func: F) -> Result<(), Error>
+where
+    F: FnOnce(&mut Task) -> Result<(), Error>,
+{
+    let res = func(task);
+    if let Err(err) = res {
+        taskmgr.restart_task(task, &err.display_causes())
+    } else {
+        Ok(())
+    }
+}
+
+/// Compress a file with xz
+fn xz(path: &Path) -> Result<(), Error> {
+    // skip already compressed files
+    if path.extension() == Some(OsStr::new("xz")) {
+        return Ok(());
+    }
+
+    let res = Command::new("xz")
+        .args(&["-9", "--force"])
+        .arg(path)
+        .stdin(Stdio::null())
+        .stdout(Stdio::null())
+        .status()
+        .context("Could not start xz")?;
+    if !res.success() {
+        bail!("xz did not finish successfully")
     }
     Ok(())
 }
