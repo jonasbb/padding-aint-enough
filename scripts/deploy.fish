508ebc1c0023677a72bb479b15c202c4092a210c
Jonas Bushart <jonas@bushart.org>
2018-07-18 16:56:27 +0200

    Update deploy script

--- scripts/deploy.fish
+++ scripts/deploy.fish
@@ -1,9 +1,12 @@
 #!/usr/bin/fish
 pushd (dirname (status --current-filename))/..
-set -l OUTDIR /mnt/data/vms/share/
 
 # Scripts working dir is the checkout directory of git
+function do
+    set -l OUTDIR "$argv[1]"
+    cp --recursive scripts ./taskmanager/tasks.db $OUTDIR
+    xsv select 2 ./alexa-top1m.????????T????.csv >$OUTDIR/alexa-top1m.txt
+end
 
-cp --recursive scripts $OUTDIR
-xsv select 2 ./alexa-top1m.????????T????.csv >$OUTDIR/alexa-top1m.txt
-
+do /mnt/data/vms/share/
+do /mnt/data/Downloads/new-task-setup/
