3602bd93170b66543beb3fd764f87739307472c6
Jonas Bushart <jonas@bushart.org>
2018-07-23 17:37:19 +0200

    Create a top 10k alexa file during deploy

--- scripts/deploy.fish
+++ scripts/deploy.fish
@@ -6,6 +6,7 @@ function do
     set -l OUTDIR "$argv[1]"
     cp --recursive scripts ./taskmanager/tasks.db $OUTDIR
     xsv select 2 ./alexa-top1m.????????T????.csv >$OUTDIR/alexa-top1m.txt
+    xsv select 2 ./alexa-top1m.????????T????.csv | head -10000 >$OUTDIR/alexa-top10k.txt
 end
 
 do /mnt/data/vms/share/
