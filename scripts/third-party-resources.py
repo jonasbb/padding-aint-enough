7cb228d8f173c3db75023e496fe212a40c4f8176
Jonas Bushart <jonas@bushart.org>
2018-10-19 17:43:19 +0200

    Tools to count domain usage
    Provided as rust tool, since the python was way too slow.
    
    This tool allows to count how often different domains are used.
    The counting works on a per label and per trace level.

---
+++ scripts/third-party-resources.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+
+import csv
+import shlex
+import subprocess
+import sys
+import typing as t
+from glob import glob
+from os import path
+
+FAILED_DOMAINS_LIST = (
+    "/mnt/data/Downloads/new-task-setup/2018-10-01-no-dnssec/failed_domains_final.csv"
+)
+CONFUSION_DOMAINS_LISTS = [
+    "/home/jbushart/projects/confusion_domains/redirects.csv",
+    "/home/jbushart/projects/encrypted-dns/results/2018-10-09-no-dnssec/confusion_domains.csv",
+]
+
+
+class Canonicalize:
+    cache: t.Dict[str, str]
+
+    def __init__(self) -> None:
+        self.cache = dict()
+        # read all files and add them to the cache
+        for file in CONFUSION_DOMAINS_LISTS:
+            rdr = csv.reader(open(file, "r"))
+            for dom, canon in rdr:
+                dom = sys.intern(dom)
+                canon = sys.intern(canon)
+                # skip comments
+                if dom.startswith("#"):
+                    continue
+                if dom in self.cache:
+                    raise Exception(
+                        f"Two duplicate entries for the same domain '{dom}' while canonicalizing."
+                    )
+                self.cache[dom] = canon
+
+    def canonicalize(self, domain: str) -> str:
+        res = domain
+        try:
+            res = self.cache[domain]
+        except KeyError:
+            pass
+        return sys.intern(res)
+
+
+def sanitize_file_name(filename: str) -> str:
+    # strip extension
+    tmp = path.basename(filename)
+    if tmp.endswith(".xz"):
+        tmp = tmp[: -len(".xz")]
+    if tmp.endswith(".json"):
+        tmp = tmp[: -len(".json")]
+    if tmp.endswith(".dnstap"):
+        tmp = tmp[: -len(".dnstap")]
+    return sys.intern(tmp)
+
+
+def load_files_to_ignore() -> t.Set[str]:
+    res = set()
+    rdr = csv.reader(open(FAILED_DOMAINS_LIST, "r"))
+    # skip header
+    next(rdr)
+    for file, _reason in rdr:
+        res.add(sanitize_file_name(file))
+    return res
+
+
+def get_label(filename: str, canonicalizer: Canonicalize) -> str:
+    # get the name of the directory containing the file
+    label = path.basename(path.dirname(filename))
+    return canonicalizer.canonicalize(label)
+
+
+def main() -> None:
+    # Load a list of files to ignore
+    # Load all the confusion domain information
+    # Load all DNS requests
+
+    canonicalizer = Canonicalize()
+    files_to_ignore = load_files_to_ignore()
+
+    # the dict key is the label, then a list of traces and each trace is a list of requests
+    loaded_domains: t.Dict[str, t.List[t.List[str]]] = dict()
+
+    for file in sorted(
+        glob(
+            "/mnt/data/Downloads/new-task-setup/2018-10-01-no-dnssec/processed/*/*.dnstap.*"
+        )
+    ):
+        if sanitize_file_name(file) in files_to_ignore:
+            continue
+        label = get_label(file, canonicalizer)
+
+        res = subprocess.run(
+            [
+                "fish",
+                "-c",
+                f"xzcat {shlex.quote(file)} | dnstap-ldns -r - | xsv search --delimiter ' ' --select 3 --no-headers 'FR' | xsv select 7-9",
+            ],
+            stdout=subprocess.PIPE,
+            check=True,
+        )
+        dns_reqs = [
+            sys.intern(req.replace(",", " "))
+            for req in res.stdout.decode("utf-8").splitlines()
+        ]
+        loaded_domains.setdefault(label, []).append(dns_reqs)
+
+    # per domain count in how many labels it appears and in how many traces
+    # the first value in the tuple is per label, the second is per trace
+    usage_per_domain: t.Dict[str, t.Tuple[t.Set[str], t.Set[str]]] = dict()
+    for label, traces in loaded_domains.items():
+        for trace_num, trace in enumerate(traces):
+            for domain in trace:
+                label_set, trace_set = usage_per_domain.setdefault(
+                    domain, (set(), set())
+                )
+                label_set.add(label)
+                trace_set.add(f"{label}-{trace_num}")
+
+    counts_per_domain: t.Dict[str, t.Tuple[int, int]] = {
+        domain: (len(labels), len(traces))
+        for domain, (labels, traces) in usage_per_domain.items()
+    }
+
+    # for each trace count how many labels have this request
+    traces_labelcount: t.List[t.List[int]] = [
+        [counts_per_domain[domain][0] for trace in traces for domain in trace]
+        for traces in loaded_domains.values()
+    ]
+    # for each trace count how many traces have this request
+    traces_tracecount: t.List[t.List[int]] = [
+        [counts_per_domain[domain][1] for trace in traces for domain in trace]
+        for traces in loaded_domains.values()
+    ]
+
+    import IPython
+
+    IPython.embed()
+
+
+if __name__ == "__main__":
+    main()
---
+++ third-party-resources/src/main.rs
@@ -0,0 +1,318 @@
+#![feature(transpose_result)]
+
+use csv::ReaderBuilder;
+use dnstap::{
+    dnstap::Message_Type,
+    process_dnstap,
+    protos::{self, DnstapContent},
+};
+use failure::{format_err, Error, ResultExt};
+use lazy_static::lazy_static;
+use log::{error, info};
+use misc_utils::fs::{file_open_read, file_open_write};
+use rayon::prelude::*;
+use serde::Deserialize;
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+    io::BufReader,
+    path::{Path, PathBuf},
+    sync::{Arc, RwLock},
+};
+use structopt::StructOpt;
+
+lazy_static! {
+    static ref CONFUSION_DOMAINS: RwLock<Arc<HashMap<String, String>>> = RwLock::default();
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+    author = "",
+    raw(setting = "structopt::clap::AppSettings::ColoredHelp")
+)]
+struct CliArgs {
+    /// Base directory containing per domain a folder which contains the dnstap files
+    #[structopt(parse(from_os_str))]
+    base_dir: PathBuf,
+    /// Some domains are known similar. Specify a CSV file renaming the "original" domain to some other identifier.
+    /// This option can be applied multiple times. It is not permitted to have conflicting entries to the same domain.
+    #[structopt(short = "d", long = "confusion_domains", parse(from_os_str))]
+    confusion_domains: Vec<PathBuf>,
+    /// List of file names which did not load properly.
+    /// Also see the `website-failed` tool.
+    #[structopt(long = "loading_failed", parse(from_os_str))]
+    loading_failed: PathBuf,
+}
+
+fn main() {
+    use std::io::{self, Write};
+
+    if let Err(err) = run() {
+        let stderr = io::stderr();
+        let mut out = stderr.lock();
+        // cannot handle a write error here, we are already in the outermost layer
+        let _ = writeln!(out, "An error occured:");
+        for fail in err.iter_chain() {
+            let _ = writeln!(out, "  {}", fail);
+        }
+        let _ = writeln!(out, "{}", err.backtrace());
+        std::process::exit(1);
+    }
+}
+
+fn run() -> Result<(), Error> {
+    // generic setup
+    env_logger::init();
+    let cli_args = CliArgs::from_args();
+
+    info!("Start loading confusion domains...");
+    prepare_confusion_domains(&cli_args.confusion_domains)?;
+    info!("Done loading confusion domains.");
+
+    info!("Start loading of failed domains...");
+    let failed_domains = prepare_failed_domains(&cli_args.loading_failed)?;
+    info!("Done loading of failed domains.");
+
+    let check_confusion_domains = make_check_confusion_domains();
+
+    // Get a list of directories
+    // Each directory corresponds to a label
+    let directories: Vec<PathBuf> = fs::read_dir(&cli_args.base_dir)?
+        .flat_map(|x| {
+            x.and_then(|entry| {
+                // Result<Option<PathBuf>>
+                entry.file_type().map(|ft| {
+                    if ft.is_dir()
+                        || (ft.is_symlink() && fs::metadata(&entry.path()).ok()?.is_dir())
+                    {
+                        Some(entry.path())
+                    } else {
+                        None
+                    }
+                })
+            })
+            .transpose()
+        })
+        .collect::<Result<_, _>>()?;
+
+    // Pairs of Label with Data (the Sequences)
+    let loaded_domains: HashMap<String, Vec<Vec<String>>> = directories
+        .into_par_iter()
+        .map(|dir| -> Result<_, Error> {
+            let label: String = check_confusion_domains(
+                &dir.file_name()
+                    .expect("Each directory has a name")
+                    .to_string_lossy(),
+            );
+
+            let mut filenames: Vec<PathBuf> = fs::read_dir(&dir)?
+                .flat_map(|x| {
+                    x.and_then(|entry| {
+                        // Result<Option<PathBuf>>
+                        entry.file_type().map(|ft| {
+                            if ft.is_file()
+                                && entry.file_name().to_string_lossy().contains(".dnstap")
+                            {
+                                // Ignore all files which are in the failed domains list
+                                let path = entry.path();
+                                let fname = path.file_name().unwrap().to_string_lossy();
+                                if !failed_domains.contains(&*fname) {
+                                    Some(entry.path())
+                                } else {
+                                    None
+                                }
+                            } else {
+                                None
+                            }
+                        })
+                    })
+                    .transpose()
+                })
+                .collect::<Result<_, _>>()?;
+            // sort filenames for predictable results
+            filenames.sort();
+
+            let responses = filenames
+                .into_iter()
+                .map(|fname| -> Result<Vec<String>, Error> {
+                    let mut events: Vec<protos::Dnstap> =
+                        process_dnstap(fname)?.collect::<Result<_, Error>>()?;
+
+                    // the dnstap events can be out of order, so sort them by timestamp
+                    // always take the later timestamp if there are multiple
+                    events.sort_by_key(|ev| {
+                        let DnstapContent::Message {
+                            query_time,
+                            response_time,
+                            ..
+                        } = ev.content;
+                        if let Some(time) = response_time {
+                            return time;
+                        } else if let Some(time) = query_time {
+                            return time;
+                        } else {
+                            panic!(
+                                "The dnstap message must contain either a query or response time."
+                            )
+                        }
+                    });
+
+                    Ok(events
+                        .into_iter()
+                        .filter_map(|ev| {
+                            let DnstapContent::Message {
+                                message_type,
+                                response_message,
+                                ..
+                            } = ev.content;
+                            match message_type {
+                                Message_Type::FORWARDER_RESPONSE => {
+                                    let (dnsmsg, _size) = response_message
+                                        .expect("Unbound always sets this: FR r msg");
+                                    let qname = dnsmsg.queries()[0].name().to_utf8();
+                                    let qtype = dnsmsg.queries()[0].query_type().to_string();
+                                    Some(format!("{} {}", qname, qtype))
+                                }
+
+                                _ => None,
+                            }
+                        })
+                        .collect())
+                })
+                .collect::<Result<Vec<Vec<String>>, Error>>()?;
+
+            Ok((label, responses))
+        })
+        .collect::<Result<_, _>>()?;
+
+    let mut usage_per_domain: HashMap<String, (HashSet<String>, HashSet<String>)> =
+        HashMap::default();
+    for (label, traces) in &loaded_domains {
+        for (trace_num, trace) in traces.iter().enumerate() {
+            for domain in trace {
+                usage_per_domain
+                    .entry(domain.clone())
+                    .and_modify(|(label_set, trace_set)| {
+                        label_set.insert(label.to_string());
+                        trace_set.insert(format!("{}-{}", label, trace_num));
+                    })
+                    .or_insert((HashSet::default(), HashSet::default()));
+            }
+        }
+    }
+    let counts_per_domain: HashMap<String, (usize, usize)> = usage_per_domain
+        .iter()
+        .map(|(label, (label_set, trace_set))| (label.clone(), (label_set.len(), trace_set.len())))
+        .collect();
+
+    let (traces_labelcount, traces_tracecount): (Vec<Vec<usize>>, Vec<Vec<usize>>) = loaded_domains
+        .values()
+        .flat_map(|traces| {
+            traces.iter().map(|trace| -> (Vec<usize>, Vec<usize>) {
+                trace
+                    .iter()
+                    .map(|domain| counts_per_domain[&**domain])
+                    .unzip()
+            })
+        })
+        .unzip();
+
+    let mut wtr = file_open_write("./traces_labelcount.json", Default::default())?;
+    serde_json::to_writer(&mut wtr, &traces_labelcount)?;
+    drop(wtr);
+    let mut wtr = file_open_write("./traces_tracecount.json", Default::default())?;
+    serde_json::to_writer(&mut wtr, &traces_tracecount)?;
+    drop(wtr);
+    let mut wtr = file_open_write("./counts_per_domain.json", Default::default())?;
+    serde_json::to_writer(&mut wtr, &counts_per_domain)?;
+    drop(wtr);
+
+    Ok(())
+}
+
+fn prepare_failed_domains(path: impl AsRef<Path>) -> Result<HashSet<String>, Error> {
+    #[derive(Debug, Deserialize)]
+    struct Record {
+        file: PathBuf,
+        reason: String,
+    }
+
+    let rdr = BufReader::new(file_open_read(path.as_ref()).with_context(|_| {
+        format!(
+            "Opening failed domains file '{}' failed",
+            path.as_ref().display()
+        )
+    })?);
+    let mut rdr = ReaderBuilder::new().has_headers(true).from_reader(rdr);
+
+    let mut failed_domains = HashSet::default();
+
+    for record in rdr.deserialize() {
+        let record: Record = record.context("Failed to read from failed domains file")?;
+        let file_name: String = (&record.file)
+            .file_name()
+            .map(|file_name| file_name.to_string_lossy().replace(".json", ".dnstap"))
+            .ok_or_else(|| {
+                format_err!(
+                    "This line does not specify a path with a file name '{}'",
+                    record.file.display()
+                )
+            })?;
+
+        failed_domains.insert(file_name);
+    }
+
+    Ok(failed_domains)
+}
+
+fn prepare_confusion_domains<D, P>(data: D) -> Result<(), Error>
+where
+    D: IntoIterator<Item = P>,
+    P: AsRef<Path>,
+{
+    #[derive(Debug, Deserialize)]
+    struct Record {
+        domain: String,
+        is_similar_to: String,
+    };
+
+    let mut conf_domains = HashMap::default();
+
+    for path in data {
+        let path = path.as_ref();
+        let mut reader = ReaderBuilder::new().has_headers(false).from_reader(
+            file_open_read(path)
+                .with_context(|_| format!("Opening confusion file '{}' failed", path.display()))?,
+        );
+        for record in reader.deserialize() {
+            let record: Record = record?;
+            // skip comment lines
+            if record.domain.starts_with('#') {
+                continue;
+            }
+            let existing = conf_domains.insert(record.domain.clone(), record.is_similar_to.clone());
+            if let Some(existing) = existing {
+                if existing != record.is_similar_to {
+                    error!("Duplicate confusion mappings for domain '{}' but with different targets: 1) '{}' 2) '{}'", record.domain, existing, record.is_similar_to);
+                }
+            }
+        }
+    }
+
+    let mut lock = CONFUSION_DOMAINS.write().unwrap();
+    *lock = Arc::new(conf_domains);
+
+    Ok(())
+}
+
+fn make_check_confusion_domains() -> impl Fn(&str) -> String {
+    let lock = CONFUSION_DOMAINS.read().unwrap();
+    let conf_domains: Arc<_> = lock.clone();
+    move |domain: &str| -> String {
+        let mut curr = domain;
+        while let Some(other) = conf_domains.get(curr) {
+            curr = other;
+        }
+        curr.into()
+    }
+}
