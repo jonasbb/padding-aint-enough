9e82577f146acb6dc73be553869c34d036174283
Jonas Bushart <jonas@bushart.org>
2018-11-07 17:38:00 +0100

    Inline fish script into the only caller

--- scripts/create-cache-dump.fish
+++ scripts/create-cache-dump.fish
@@ -3,7 +3,8 @@ pushd (dirname (status --current-filename))
 set -l PREFETCH_DOMAINS ./alexa-top30k-eff-tlds.txt
 
 # flush old cache entries
-./flush-unbound.fish >/dev/null 2>&1
+sudo unbound-control reload >/dev/null 2>&1
+sleep 1
 # Reload with new data, run double to ensure no SERVFAILS or similar are returned
 # Second one should be very quick anyways
 ../dns-par --file "$PREFETCH_DOMAINS" --limit-parallel 20 --servers "127.0.0.1" >/dev/null 2>&1
--- scripts/flush-unbound.fish
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/fish
-
-sudo unbound-control reload
-sleep 2
