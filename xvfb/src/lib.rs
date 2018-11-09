e09b82475c41fb2428c700fc7f50945f915e785f
Jonas Bushart <jonas@bushart.org>
2018-11-09 10:47:11 +0100

    Add tiny crate to spawn and control a xvfb instance

---
+++ xvfb/src/lib.rs
@@ -0,0 +1,83 @@
+extern crate log;
+
+use log::{debug, error, warn};
+use std::{
+    fmt,
+    io::{self, Read},
+    process::{Child, Command, Stdio},
+    str::FromStr,
+};
+
+#[derive(Debug)]
+pub struct Xvfb {
+    /// Handle to the running Xvfb process
+    process: Child,
+    /// The X display opened by this instance of Xvfb
+    display: XDisplay,
+}
+
+impl Xvfb {
+    pub fn new() -> io::Result<Self> {
+        let mut process = Command::new("Xvfb")
+            .args(&["-displayfd", "1"])
+            .stdin(Stdio::null())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()?;
+        let mut display_num = String::with_capacity(5);
+        let display = match process.stdout.as_mut() {
+            Some(stdout) => {
+                stdout.read_to_string(&mut display_num)?;
+                XDisplay(u16::from_str(display_num.trim()).unwrap())
+            }
+            None => {
+                error!("No stdout");
+                unimplemented!()
+            }
+        };
+        Ok(Xvfb { process, display })
+    }
+
+    pub fn get_display(&self) -> XDisplay {
+        self.display
+    }
+}
+
+impl Drop for Xvfb {
+    fn drop(&mut self) {
+        match self.process.kill() {
+            Ok(()) => debug!("Stopped Xvfb process {}", self.process.id()),
+            Err(err) => {
+                // InvalidInput is raise, if process was already dead
+                if err.kind() != io::ErrorKind::InvalidInput {
+                    warn!(
+                        "Failed to stop Xvfb process {} due to {}",
+                        self.process.id(),
+                        err
+                    )
+                }
+            }
+        }
+    }
+}
+
+/// Represents an X display number
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub struct XDisplay(u16);
+
+impl fmt::Display for XDisplay {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, ":{}", self.0)
+    }
+}
+
+#[test]
+fn test_create_xvfb() {
+    assert!(Xvfb::new().is_ok());
+}
+
+#[test]
+fn test_x_display() {
+    let xvfb = Xvfb::new().unwrap();
+    assert!(format!("{}", xvfb.get_display()).starts_with(':'));
+}
