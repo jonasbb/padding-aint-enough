8db8b8cf19c76cdf9ce6057d51b6ab7bc67452af
Jonas Bushart <jonas@bushart.org>
2019-02-25 11:17:04 +0100

    Remove impl Trait from argument position
    This allows to use the turbofish for type hints

--- countermeasures/src/utils.rs
+++ countermeasures/src/utils.rs
@@ -6,18 +6,20 @@ use std::future::Future as StdFuture;
 
 /// Converts from an old style Future to a new style one:
 #[allow(dead_code)]
-pub(crate) fn forward<I, E>(
-    f: impl Future<Item = I, Error = E> + Unpin,
-) -> impl StdFuture<Output = Result<I, E>> {
+pub(crate) fn forward<F, I, E>(f: F) -> impl StdFuture<Output = Result<I, E>>
+where
+    F: Future<Item = I, Error = E> + Unpin,
+{
     use tokio_async_await::compat::forward::IntoAwaitable;
     f.into_awaitable()
 }
 
 /// Converts from a new style Future to an old style one:
 #[allow(dead_code)]
-pub(crate) fn backward<I, E>(
-    f: impl StdFuture<Output = Result<I, E>>,
-) -> impl Future<Item = I, Error = E> {
+pub(crate) fn backward<F, I, E>(f: F) -> impl Future<Item = I, Error = E>
+where
+    F: StdFuture<Output = Result<I, E>>,
+{
     use tokio_async_await::compat::backward;
     backward::Compat::new(f)
 }
