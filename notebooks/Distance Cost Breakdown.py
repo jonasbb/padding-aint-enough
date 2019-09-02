a2adae9c753f995a3809317c6be532ad7ae52edf
Jonas Bushart <jonas@bushart.org>
2019-09-02 13:37:52 +0200

    Add plots to show the breakdown of the distance cost of DNS sequences

---
+++ notebooks/Distance Cost Breakdown.py
@@ -0,0 +1,98 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.2.0+dev
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import typing as t
+from itertools import chain, combinations_with_replacement
+
+import matplotlib.pyplot as plt
+import pylib
+
+# %%
+K = t.TypeVar("K")
+V = t.TypeVar("V")
+
+# %%
+# %matplotlib inline
+plt.rcParams["figure.figsize"] = [25, 15]
+
+# %%
+s0 = pylib.load_file("/mnt/data/Downloads/dnscaptures-main-group/google.com/google.com-0-0.dnstap.xz")
+s1 = pylib.load_file("/mnt/data/Downloads/dnscaptures-main-group/google.com/google.com-1-0.dnstap.xz")
+
+# %%
+s0
+
+# %%
+s0.distance(s1)
+
+# %%
+s0.distance_with_details(s1)
+
+
+# %%
+def merge_dict_counts(a: t.Dict[K, int], b: t.Dict[K, int]) -> t.Dict[K, int]:
+    res = {}
+    for key in chain(a.keys(), b.keys()):
+        res[key] = a.get(key, 0) + b.get(key, 0)
+    return res
+
+
+# %%
+def merge_costs_to_lists(l: t.Dict[K, t.List[V]], costs: t.Dict[K, V]) -> None:
+    for key, value in costs.items():
+        l.setdefault(key, list()).append(value)
+
+
+# %%
+def normalize_dict(d: t.Dict[K, int], r: int) -> t.Dict[K, float]:
+    res = {}
+    for key, value in d.items():
+        res[key] = value / r
+    return res
+
+
+# %%
+sequences = pylib.load_folder("/mnt/data/Downloads/dnscaptures-main-group/")
+
+# %%
+lists: t.Dict[str, t.List[float]] = {}
+total = []
+for domain, seqs in sequences[:]:
+    for a, b in combinations_with_replacement(seqs, 2):
+        l = max(a.len(), b.len())
+        mc: t.Tuple[int, t.Dict[str, int]] = a.distance_with_details(b)
+        costs = normalize_dict(mc[1], l)
+        merge_costs_to_lists(lists, costs)
+        total.append(mc[0] / l)
+lists.keys(), len(total)
+
+# %%
+lists["total"] = total
+labels = lists.keys()
+values = lists.values()
+plt.plot([0, len(values)+1], [0, 0], color="black", alpha=0.2)
+plt.boxplot(values, labels=labels)
+plt.ylim(bottom=-0.1, top=6)
+plt.title("Normalized Distances")
+plt.savefig(f"distance-cost-distribution-{len(total)}.svg")
+plt.savefig(f"distance-cost-distribution-{len(total)}.png")
+plt.show()
+
+# %%
+a = 1
+
+
+# %%
