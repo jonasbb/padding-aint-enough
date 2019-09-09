Update cluster script with correct distance matrix calculation

scripts/cluster.py
@@ -5,23 +5,44 @@ import typing as t
 from glob import glob
 
 import pylib
+import scipy.cluster.hierarchy as cluster
 from matplotlib import pyplot as plt
-from scipy.cluster.hierarchy import dendrogram, linkage
+from natsort import natsorted
+from scipy.spatial.distance import pdist
 
 
 def labels(sequences: t.List[pylib.Sequence]) -> t.List[str]:
     return [os.path.basename(seq.id()).replace(".dnstap.xz", "") for seq in sequences]
 
 
-def main() -> None:
-    files = glob(os.path.join(sys.argv[1], "*", "*dnstap*"))
-    seqs = sorted((pylib.load_file(file) for file in files), key=lambda seq: seq.id())
+def help(pgrm: str) -> None:
+    print(
+        f"""Usage: ./{pgrm} PATTERN [PATTERN [...]]
 
-    dists = []
-    for i, s1 in enumerate(seqs):
-        for j, s2 in enumerate(seqs):
-            if i < j:
-                dists.append(s1.distance(s2))
+    Each PATTERN is either the path to a file loadable with `pylib.load_file`
+    or a glob pattern for a set of files."""
+    )
+    sys.exit(1)
+
+
+def main() -> None:
+    if len(sys.argv) < 2:
+        help(sys.argv[0])
+
+    files: t.List[str] = []
+    for arg in sys.argv[1:]:
+        files += glob(arg)
+    sequences = natsorted(
+        (pylib.load_file(file) for file in files), key=lambda seq: seq.id()
+    )
+
+    # pdist requires a 2-dimensional array, without any good reason
+    # So convert the list into an list of 1-element lists to fullfill this requirement
+    # The comparison lambda just has to take the 0th element every time
+    sequences_matrix = [[s] for s in sequences]
+    distances_pairwise = pdist(
+        sequences_matrix, lambda a, b: a[0].distance(b[0]) / max(a[0].len(), b[0].len())
+    )
 
     for (threshold, method) in [
         (2000, "single"),
@@ -31,15 +52,16 @@ def main() -> None:
         # (3000, "median"),
         (6000, "ward"),
     ]:
-        Z = linkage(dists, method=method, optimal_ordering=True)
+        Z = cluster.linkage(distances_pairwise, method=method, optimal_ordering=True)
         _fig = plt.figure(figsize=(15, len(files) * 0.15))
-        _dn = dendrogram(
+        _dn = cluster.dendrogram(
             Z,
-            labels=labels(seqs),
-            orientation="left",
-            show_leaf_counts=True,
-            show_contracted=True,
             color_threshold=threshold,
+            distance_sort="ascending",
+            labels=labels(sequences),
+            orientation="right",
+            show_contracted=True,
+            show_leaf_counts=True,
         )
         plt.savefig(f"cluster-{method}.svg", bbox_inches="tight")
 