59bd7a653909db9705619b72f6831cdef27368aa
Jonas Bushart <jonas@bushart.org>
2019-09-11 17:20:30 +0200

    Run shellcheck

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-pushd $(dirname $(readlink -f $0))
+pushd "$(dirname "$(readlink -f "$0")")" || exit
 docker build -f Dockerfile -t debian-rust-nightly --rm .
--- taskmanager/docker/bin/capture-output.sh
+++ taskmanager/docker/bin/capture-output.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-stdbuf -i0 -o0 -e0 $1 | ts | tee $2
+stdbuf -i0 -o0 -e0 "$1" | ts | tee "$2"
