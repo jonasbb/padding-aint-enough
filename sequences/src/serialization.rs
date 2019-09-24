8dfd0a1d39653a0aa214579cfb209afe61f6a79b
Jonas Bushart <jonas@bushart.org>
2019-09-24 16:47:37 +0200

    Simplify serialization code

--- sequences/src/serialization.rs
+++ sequences/src/serialization.rs
@@ -1,12 +1,9 @@
 use misc_utils::{Max, Min};
 use num_traits::float::Float;
 use ordered_float::NotNan;
-use serde::de::{Deserializer, Error, Visitor};
-use std::{
-    fmt::{self, Display},
-    marker::PhantomData,
-    str::FromStr,
-};
+use serde::de::{Deserializer, Error};
+use serde_with::rust::display_fromstr;
+use std::{fmt::Display, str::FromStr};
 
 /// Deserialize T using [FromStr]
 pub fn deserialize_min_notnan<'de, D, T>(deserializer: D) -> Result<Min<NotNan<T>>, D::Error>
@@ -15,9 +12,9 @@ where
     T: FromStr + Float,
     T::Err: Display,
 {
-    Ok(Min::with_initial(
-        deserializer.deserialize_str(NotNanHelper(PhantomData))?,
-    ))
+    let value: T = display_fromstr::deserialize(deserializer)?;
+    let value = NotNan::new(value).map_err(Error::custom)?;
+    Ok(Min::with_initial(value))
 }
 
 /// Deserialize T using [FromStr]
@@ -27,29 +24,7 @@ where
     T: FromStr + Float,
     T::Err: Display,
 {
-    Ok(Max::with_initial(
-        deserializer.deserialize_str(NotNanHelper(PhantomData))?,
-    ))
-}
-
-struct NotNanHelper<S>(PhantomData<S>);
-
-impl<'de, S> Visitor<'de> for NotNanHelper<S>
-where
-    S: FromStr + Float,
-    <S as FromStr>::Err: Display,
-{
-    type Value = NotNan<S>;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        write!(formatter, "valid json object")
-    }
-
-    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
-    where
-        E: Error,
-    {
-        let s: S = value.parse().map_err(Error::custom)?;
-        NotNan::new(s).map_err(Error::custom)
-    }
+    let value: T = display_fromstr::deserialize(deserializer)?;
+    let value = NotNan::new(value).map_err(Error::custom)?;
+    Ok(Max::with_initial(value))
 }
